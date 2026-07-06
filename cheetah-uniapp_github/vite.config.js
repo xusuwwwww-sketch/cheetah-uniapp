83cd0fe73b26ab209238c0b3e28acf730d67c417
Product Agent <product-agent@cheetah.ai>
2026-07-06 16:16:08 +0800

    perf: 开启terser JS压缩，通过微信代码质量检测

---
+++ cheetah-uniapp_github/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+
+export default defineConfig({
+  plugins: [uni()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+})
